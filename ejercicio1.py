03cf0a370dcf789994e4eda1cbff27f98dbb6ddd
oswaldaguilera <oswald@DESKTOP-RCVTKK2.localdomain>
2020-06-22 07:07:00 -0500

    Conteos

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,10 +1,58 @@
-from pyspark.sql import SparkSession  
+from pyspark.sql import SparkSession 
+from pyspark import SparkContext, SparkConf
+from pyspark.sql import SparkSession
+from pyspark.sql import Row
+from pyspark.sql.types import StructField
+from pyspark.sql.types import StructType
+from pyspark.sql.types import StringType
+from pyspark.sql.types import DoubleType
+from pyspark.sql.types import IntegerType
+import matplotlib.pyplot as plt
+from array import array
+import pandas as pd
+
+
+
 
 scSpark = SparkSession \
     .builder \
-    .appName("Python Spark SQL basic example: Reading CSV file without mentioning schema") \
+    .appName("Ejercicio 1") \
     .config("spark.some.config.option", "some-value") \
     .getOrCreate()
 
 sdfData = scSpark.read.csv("/home/oswald/data/all_data.csv", header=True, sep=",")
-sdfData.show()
+sdfData.createOrReplaceTempView("profeco")
+
+
+#Conteo de registros
+conteoTotal = scSpark.sql("SELECT count(1) from profeco")
+conteoTotal.show()
+
+
+#Conteo de categorías
+conteoCategorias = scSpark.sql("SELECT categoria, count(distinct producto) as conteo from profeco group by categoria order by conteo asc")
+conteoCategorias = conteoCategorias.toPandas()
+
+
+ax = conteoCategorias.plot.barh(x='categoria', y='conteo', legend=True, rot = 0)
+plt.tight_layout()
+plt.rcParams['legend.title_fontsize'] = 'xx-small'
+plt.savefig('categoriasBarChart.png')
+
+
+#Cadenas comerciales monitoreadas
+conteoComercial = scSpark.sql("SELECT cadenaComercial, count(distinct producto) as conteo from profeco group by cadenaComercial")
+conteoComercial = conteoComercial.toPandas()
+conteoComercial
+ax = conteoComercial.plot.barh(x='cadenaComercial', y='conteo', legend=True, rot = 0)
+plt.tight_layout()
+plt.rcParams['legend.title_fontsize'] = 'xx-small'
+plt.savefig('comercialBarChart.png')
+
+conteoComercial.head()
+
+#Productos mas monitoreados por cadena
+conteoProducto = scSpark.sql("SELECT estado, producto, count(1) as conteo from profeco group by estado, producto order by conteo desc")
+conteoProducto = conteoProducto.toPandas()
+
+conteProductoEstado = conteoProducto.groupby('estado').head(5)
